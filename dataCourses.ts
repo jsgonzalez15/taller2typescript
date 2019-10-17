15fa5fbc6a54814ccd037e4fb612de2ba4ae77a0
jsgonzalez15 <js.gonzalez15@uniandes.edu.co>
2019-10-17 13:01:53 -0500

    1 file modified, 2 added

---
+++ dataCourses.ts
@@ -0,0 +1,7 @@
+export const dataCourses = [
+  {"name": "Ingeniería de Sw", "teacher": {name:"Pablo Picasso", numOffice:"X-102", code:445645643, cardId:62435234234,university:"Universidad de Los Andes"}, "credits": 4},
+  {"name": "Futbol 1", "teacher": {name:"Freddy Rincón", numOffice:"ML-911", code:3453453, cardId:856785678,university:"Universidad de Los Andes"}, "credits": 2},
+  {"name": "Algoritmos", "teacher": {name:"Carlos Fuentes", numOffice:"T-052", code:30012345, cardId:4345456,university:"Universidad de Los Andes"}, "credits": 2},
+  {"name": "Estructuras de Datos", "teacher": {name:"Yesid D", numOffice:"A-402", code:192320383, cardId:56974523,university:"Universidad de Los Andes"}, "credits": 1},
+  {"name": "Futbol 2", "teacher": {name:"James R", numOffice:"GA-724", code:23492234, cardId:87669878,university:"Universidad de Los Andes"},  "credits": 6},
+]
---
+++ dataStudent.ts
@@ -0,0 +1,10 @@
+export const dataStudent =
+{
+  "name": "Ana María Suarez",
+  "code": 20182010,
+  "cardId": 231313123,
+  "age": 25,
+  "address": "Calle falsa 123",
+  "phone": "01800213213",
+  "currentCourses": []
+}
